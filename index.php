d50b8d647e2cba09e3151e9049745ddc612ccdd6
Nweke273 <chisom5710@gmail.com>
2021-11-12 03:07:23 -0800

    get ready index page

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>yea</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+	<h1>You are always welcome</h1>
+	<script src="file.js"></script>
+
+	<p>Who is <span>your</span> parent</p>
+	<div class="box">
+		
+	</div>
+	<p id="text">This is <b>what<b> i could think of</p>
+	<button id="btn">Show Text</button>
+	
+</body>
+
+</html>
