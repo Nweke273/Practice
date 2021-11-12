5ad5ee547b86706687579bc3158f5215e46694d6
Nweke273 <chisom5710@gmail.com>
2021-11-12 03:17:54 -0800

    add Readme and javascript file

---
+++ file.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    
+
+  $("#btn").click(function(){
+  
+    $(".box").load("readme.txt", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+          alert("External content loaded successfully!");
+        if(statusTxt == "error")
+          alert("Error: " + xhr.status + ": " + xhr.statusText);
+      });
+  });
+
+});
